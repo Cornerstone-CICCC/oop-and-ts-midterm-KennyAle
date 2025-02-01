bf96bef1b0f536345df62b87c3c4bfc49d4caa95
KennyAle <kennyviver@gmail.com>
2025-02-01 15:27:36 -0800

    FIX Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,12 @@
 import { Component } from "../common/Component.js";
-import toast, { Toaster } from 'react-hot-toast';
-
 
 export class Header extends Component {
   render() {
-    const notify = () => toast('Here is your toast.');
     const header = document.createElement('div')
     header.innerHTML = `
       <h2>${this.props.siteName}</h2>
       <input type="text" placeholder="Jackets, Backpacks, Raincoats..." class="search-bar">
-      <button onClick=${notify} class="open-cart-btn">
-      <Toaster />
+      <button class="open-cart-btn">
       Cart<span class="cart-count">0</span></button>
     `
     return header
